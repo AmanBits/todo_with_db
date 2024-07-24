5f11c38562d82fbbe62c1cf7c08d1c00dfa2fd50
Mohammad Aman <mhdaman60@gmail.com>
2024-07-24 13:01:09 +0530

    Title box css changed

--- client/TodoApp/src/components/AppTitle.jsx
+++ client/TodoApp/src/components/AppTitle.jsx
@@ -2,7 +2,7 @@ import style from '../assets/css/apptitle.module.css';
 const AppTitle = () =>{
    return(
     <div className={style.titlebox}>
-        <h1 className={style.h1}>My List</h1>
+        <h1 className={style.h1}>+WishLists</h1>
      
     </div>
    )
